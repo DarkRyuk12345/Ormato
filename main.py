27be13cfd517cd13027d264b8e91656d0078212e
Arthex12345 <87478573+DarkRyuk12345@users.noreply.github.com>
2022-05-18 20:29:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,15 +15,6 @@ class Ormato:
         self.helpers = hs
         self.types = ty
         self.interface = Interface
-        
-
-            
-
-
-
-
-    
-
 
 print(Ormato().types().User(5, 8372717).location["city"])
 print(Ormato().interface().startup(Ormato().types.User(22, 55)))
